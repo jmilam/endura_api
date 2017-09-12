bc1eba2c4c49ef15ec524bbc47f289a62a99aa3b
Jason Milam <jasonlmilam@gmail.com>
2017-09-12 10:08:56 -0400

    Update url for marketing emails

--- app/mailers/marketing_mailer.rb
+++ app/mailers/marketing_mailer.rb
@@ -1,6 +1,8 @@
 class MarketingMailer < ApplicationMailer
 	def notify_tsm_new_order(from_email, to_email, user, order, items)
+			@url = Rails.env == "production" ? "http://marketing.enduraproducts.com" : "http://marketing_test.enduraproducts.com"
 			@from_email = from_email
+			@from_email << ", dsavage@enduraproducts.com"
 			@user = user
 			@items = items
 			@order = order
@@ -10,11 +12,12 @@ class MarketingMailer < ApplicationMailer
 	end
 
 	def notify_rep_order_status(from_email, to_email, user, order)
+		@url = Rails.env == "production" ? "http://marketing.enduraproducts.com" : "http://marketing_test.enduraproducts.com"
 		@from_email = from_email
 		@user = user
 		@order = order
 		@status = @order['accepted'] ? "Approved" : "Denied"
 
-		mail(from: "order_status@enduraproducts.com", to: to_email, subject: "Order ##{@order['id']} was #{@status}")
+		mail(from: "order_status@enduraproducts.com", to: to_email, cc: 'dsavage@enduraproducts.com' subject: "Order ##{@order['id']} was #{@status}")
 	end
 end
