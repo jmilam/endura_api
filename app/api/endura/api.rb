ddffa3f0152ca75f893bdf27fb5e7c567bcbb1a2
Jason Milam <jasonlmilam@gmail.com>
2017-09-25 00:15:14 -0400

    Fix Token issue with json format

--- app/api/endura/api.rb
+++ app/api/endura/api.rb
@@ -510,7 +510,7 @@ class Endura::API < Grape::API
 
 		  desc 'Send email for Image Request when order checked out'
 		  post :new_image_request do
-		  	MarketingMailer.new_image_request(JSON.parse(params[:request])).deliver
+		  	MarketingMailer.new_image_request(params[:order_id])).deliver
 		  	{success: true}
 		  end
 		end
