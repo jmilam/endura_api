f0fd97f8b9097403ce53d06842c8ee8d8ef38e1f
Jason Milam <jasonlmilam@gmail.com>
2017-10-12 09:01:30 -0400

    Change marketing test url

--- app/mailers/marketing_mailer.rb
+++ app/mailers/marketing_mailer.rb
@@ -1,6 +1,6 @@
 class MarketingMailer < ApplicationMailer
 	def notify_tsm_new_order(from_email, to_email, user, order, items)
-			@url = Rails.env == "production" ? "http://marketing.enduraproducts.com" : "http://marketing_test.enduraproducts.com"
+			@url = Rails.env == "production" ? "http://marketing.enduraproducts.com" : "http://marketing-test.enduraproducts.com"
 			@from_email = from_email
 			@from_email << ", marketing@enduraproducts.com, kcoltrane@enduraproducts.com"
 			@user = user
@@ -12,7 +12,7 @@ class MarketingMailer < ApplicationMailer
 	end
 
 	def notify_rep_order_status(from_email, to_email, user, order)
-		@url = Rails.env == "production" ? "http://marketing.enduraproducts.com" : "http://marketing_test.enduraproducts.com"
+		@url = Rails.env == "production" ? "http://marketing.enduraproducts.com" : "http://marketing-test.enduraproducts.com"
 		@from_email = from_email
 		@user = user
 		@order = order
@@ -22,7 +22,7 @@ class MarketingMailer < ApplicationMailer
 	end
 
 	def notify_tsm_past_due_orders(from_email, to_email, user, order, items)
-			@url = Rails.env == "production" ? "http://marketing.enduraproducts.com" : "http://marketing_test.enduraproducts.com"
+			@url = Rails.env == "production" ? "http://marketing.enduraproducts.com" : "http://marketing-test.enduraproducts.com"
 			@from_email = from_email
 			@from_email << ", marketing@enduraproducts.com, kcoltrane@enduraproducts.com"
 			@user = user
@@ -34,18 +34,18 @@ class MarketingMailer < ApplicationMailer
 	end
 
 	def new_catalog_request(catalog_request_ids)
-		@url = Rails.env == "production" ? "http://marketing.enduraproducts.com" : "http://marketing_test.enduraproducts.com"
+		@url = Rails.env == "production" ? "http://marketing.enduraproducts.com" : "http://marketing-test.enduraproducts.com"
 		catalog_request_ids.each do |id|
 			@id = id
 
-			mail(from: "marketing_ecommerce@enduraproducts.com", to: "wrike+into178296025@wrike.com", cc: 'marketing@enduraproducts.com, kcoltrane@enduraproducts.com' , subject: "A new Catalog Request form has been submitted.")
+			mail(from: "marketing_ecommerce@enduraproducts.com", to: "wrike+into178296025@wrike.com", cc: 'marketing@enduraproducts.com, kcoltrane@enduraproducts.com' , subject: "[A new Catalog Request form has been submitted.]")
 		end
 	end
 
 	def new_image_request(order_id)
-		@url = Rails.env == "production" ? "http://marketing.enduraproducts.com" : "http://marketing_test.enduraproducts.com"
+		@url = Rails.env == "production" ? "http://marketing.enduraproducts.com" : "http://marketing-test.enduraproducts.com"
 		@order_id = order_id
 
-		mail(from: "marketing_ecommerce@enduraproducts.com", to: "wrike+into178296356@wrike.com", cc: 'dsavage@enduraproducts.com' , subject: "A new Image Request has been submitted.")
+		mail(from: "marketing_ecommerce@enduraproducts.com", to: "wrike+into178296356@wrike.com", cc: 'dsavage@enduraproducts.com' , subject: "[A new Image Request has been submitted.]")
 	end
 end
